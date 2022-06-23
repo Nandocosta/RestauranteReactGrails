762ffd48f43611469a6b3e9fa47af9863174ed66
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-06-23 15:24:01 -0300

    Axios adicionando

---
+++ server/src/test/groovy/comum/RequestMapSpec.groovy
@@ -0,0 +1,18 @@
+package comum
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class RequestMapSpec extends Specification implements DomainUnitTest<RequestMap> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
