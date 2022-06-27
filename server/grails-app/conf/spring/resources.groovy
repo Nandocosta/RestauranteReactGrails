8b0a15b5f581ee2174dbc5e3613b974b31958aba
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-06-27 16:20:05 -0300

    Criando userPasswordEncoderListener

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,7 @@
+import com.restaurantereactgrails.UserPasswordEncoderListener
+
 // Place your Spring DSL code here
 beans = {
+//    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
