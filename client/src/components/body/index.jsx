8c216fc3b2693e046bcb945a712d585a597ad7f3
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-07-14 15:38:51 -0300

    Refatorando página home e criando component Body

--- client/src/components/body/index.jsx
+++ client/src/components/body/index.jsx
@@ -1,1 +1,61 @@
 import React from 'react'
+
+import './index.css'
+
+import {
+    HomeOutlined,
+    UserOutlined,
+    ShopOutlined,
+    SearchOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+
+const { Header, Sider, Content } = Layout;
+
+export default function Body() {
+    return(
+        <div>
+            <Layout >
+                <Sider className="style-sider-menu" >
+                    <div className="logo" />
+                    <Menu
+                        className="style-menu"
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        items={[
+                            {
+                                key: '1',
+                                icon: <HomeOutlined />,
+                                label: 'Home',
+                            },
+                            {
+                                key: '2',
+                                icon: <ShopOutlined />,
+                                label: 'Produtos',
+                            },
+                            {
+                                key: '3',
+                                icon: <UserOutlined/>,
+                                label: 'Usuario',
+                            },
+                        ]}
+                    />
+                </Sider>
+                <Layout className="site-layout-body">
+                    <Header className="site-layout-header">
+                        <div className='seach-container'>
+                            <input className='input-search' type="text" name='search'/>
+                            <button className='input-searh-button'>
+                                <SearchOutlined />
+                            </button>
+                        </div>
+                    </Header>
+                    <Content className="site-layout-background" >
+
+                    </Content>
+                </Layout>
+            </Layout>
+        </div>
+    )
+}
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,19 +1,8 @@
-import React, {useEffect, useState} from 'react';
-import './index.css'
-
-import {
-    HomeOutlined,
-    UserOutlined,
-    ShopOutlined,
-    SearchOutlined,
-} from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import React, {useEffect} from 'react';
 
 import Api from "../../services/Api";
 import Auth from "../../security/Auth";
-import Tabela from "../../components/tabela";
-
-const { Header, Sider, Content } = Layout;
+import Body from "../../components/body";
 
 function Home () {
 
@@ -31,47 +20,7 @@ function Home () {
     return (
         <>
             <div>
-                <Layout >
-                    <Sider className="style-sider-menu" >
-                        <div className="logo" />
-                        <Menu
-                            className="style-menu"
-                            theme="dark"
-                            mode="inline"
-                            defaultSelectedKeys={['1']}
-                            items={[
-                                {
-                                    key: '1',
-                                    icon: <HomeOutlined />,
-                                    label: 'Home',
-                                },
-                                {
-                                    key: '2',
-                                    icon: <ShopOutlined />,
-                                    label: 'Produtos',
-                                },
-                                {
-                                    key: '3',
-                                    icon: <UserOutlined/>,
-                                    label: 'Usuario',
-                                },
-                            ]}
-                        />
-                    </Sider>
-                    <Layout className="site-layout-body">
-                        <Header className="site-layout-header">
-                            <div className='seach-container'>
-                                <input className='input-search' type="text" name='search'/>
-                                <button className='input-searh-button'>
-                                    <SearchOutlined />
-                                </button>
-                            </div>
-                        </Header>
-                        <Content className="site-layout-background" >
-
-                        </Content>
-                    </Layout>
-                </Layout>
+                <Body/>
             </div>
         </>
     );
